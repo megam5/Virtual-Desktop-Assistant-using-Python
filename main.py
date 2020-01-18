3fb8c0f3659451e46b2c5b4f9b8e8ade750a9a8c
Gul Ali Mafat <55942773+megam5@users.noreply.github.com>
2020-01-18 19:06:15 +0530

    to be continued.....

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # import packages
-import pyttsx3                              # text to speech
+import pyttsx3                              # text to speech (https://github.com/nateshmbhat/pyttsx3)
 import speech_recognition as sr             # speech recognition
-import datetime                             
-import wikipedia
-import webbrowser
+import datetime                             # access date and time
+import wikipedia                            # access wikipedia content from the python program
+import webbrowser                           # access web browser
 import os
 import smtplib
 
-print('Initializing Amili')
 
-engine = pyttsx3.init()                     # object creation
+
+print('Initializing A')
+
+MASTER = 'Ali'
+
+engine = pyttsx3.init()                         # object creation
 
 """RATE"""
 # rate = engine.getProperty('rate')             # getting details of current speaking rate
@@ -18,19 +22,80 @@ engine.setProperty('rate', 140)                 # setting up new voice rate
 # rate = engine.getProperty('rate')             # getting details of current speaking rate
 
 """VOICE"""
-voices = engine.getProperty('voices')           #getting details of current voice
-engine.setProperty('voice', voices[0].id)       #changing index, changes voices. o for male
-# engine.setProperty('voice', voices[1].id)     #changing index, changes voices. 1 for female
+voices = engine.getProperty('voices')           # getting details of current voice
+engine.setProperty('voice', voices[0].id)       # changing index, changes voices. o for male
+# engine.setProperty('voice', voices[1].id)     # changing index, changes voices. 1 for female
 
-"""STRING TO SPEECH"""
-def speak(text):                                # define a function
-    engine.say(text)                            #   
+
+
+"""Function to pronounce any string"""
+def speak(text):
+    engine.say(text)                            
     engine.runAndWait()
 
-""" """    
+"""Wishing after Initialization"""
 def wishMe():
-    hour = datetime.datetime.now().hour
-    print(hour)
+    # import datetime module to insert time
+    hour = int(datetime.datetime.now().hour)            # select time (hour)
+    minute = datetime.datetime.now().time().minute      # select time (minutes)
+    print(f'Time is {hour}:{minute}')
 
-speak('Initializing Amili...')
+    # conditions for morning, afternoon, evening
+    if hour>=0 and hour<12:
+        speak('Good Morning' + MASTER)
+
+    elif hour>=12 and hour<18:
+        speak('Good Afternoon' + MASTER)
+
+    else:
+        speak('Good Evening' + MASTER)
+
+    # speak time (hour and minute)
+    speak(f'I am A. The time is {hour} hours and {minute} minutes.')
+
+    # after wishing
+    speak('How may I help you?')
+
+"""Function for taking commands from microphone""" 
+def takeCommand():
+    # https://www.simplifiedpython.net/speech-recognition-python/
+    r = sr.Recognizer()                         # recognize voice
+    with sr.Microphone() as source:             # source of voice
+        print('Listening...')
+        audio = r.listen(source)                # listen to the source and store it in the audio
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')     # recognizer to convert audio to text
+        print(f'You said {query}\n')
+
+    except:
+        print('I didn\'t understand what you said.Can you please repeat.')
+        query = None
+    
+    return query
+
+
+speak('Initializing A.....')
 wishMe()
+query = takeCommand()
+
+"""Execution of Wikipedia query"""
+if 'wikipedia' in query.lower():                           # search 'Wikipedia' word in query
+    speak('Searching Wikipedia.....')                      # if found (speak)
+    query = query.replace('Wikipedia', '')                 # remove 'Wikipedia' from query to search only for desired word
+    results = wikipedia.summary(query, sentences=2)
+    print(results)
+
+elif 'open youtube' in query.lower():
+    url = 'youtube.com'
+    # Windows path for chrome
+    chrome_path = 'C:/Users/HCL/AppData/Local/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+
+elif 'google it' in query.lower():
+    speak('Wait for Google results.....')
+    query = query.replace('Google it', '')
+    url = "http://www.google.com/?#q="
+    chrome_path = 'C:/Users/HCL/AppData/Local/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url + query)
